af413cff56aca120b4fdffa446956bea01ec30a6
Gabriele <gabriele@kuama.net>
2021-05-17 18:18:02 +0200

    wip

--- ios/Classes/SwiftFlutterLocationListenerPlugin.swift
+++ ios/Classes/SwiftFlutterLocationListenerPlugin.swift
@@ -2,9 +2,12 @@ import Flutter
 import UIKit
 import ios_location_listener
 import Combine
-
+import CoreLocation
 public class SwiftFlutterLocationListenerPlugin: NSObject, FlutterPlugin {
+    var backgroundMethodChannel : FlutterMethodChannel? = nil
+    var binaryMessage : FlutterBinaryMessenger? = nil
   public static func register(with registrar: FlutterPluginRegistrar) {
+    self.binaryMessage = registrar.messenger()
     let channel = FlutterMethodChannel(name: "flutter_location_listener", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterLocationListenerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
@@ -13,6 +16,7 @@ public class SwiftFlutterLocationListenerPlugin: NSObject, FlutterPlugin {
   let stream = StreamLocation()
   var cancellable: AnyCancellable? = nil
   var location: CLLocation? = nil
+    
     
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     print("handle: \(call.method) \(call.arguments)")
@@ -28,21 +32,42 @@ public class SwiftFlutterLocationListenerPlugin: NSObject, FlutterPlugin {
         preferences.synchronize()
         
         stream.startUpdatingLocations()
-        DispatchQueue.main.async{
-            self.cancellable = self.stream.subject?.sink {
+            self.cancellable = self.stream.subject?.subscribe(on: DispatchQueue.global()).sink {
             s in
               self.location = s
-              print("\(s.coordinate.latitude)-\(s.coordinate.longitude)")
-          }
-        }
+                print("\(s.coordinate.latitude)-\(s.coordinate.longitude)")
+                
+                if (backgroundMethodChannel == nil) {
 
-        result(nil)
+//                    val flutterLoader = FlutterLoader()
+//                           flutterLoader.startInitialization(context)
+//                           flutterLoader.ensureInitializationComplete(context, null)
+//
+//                           val userCallbackInformation = FlutterCallbackInformation.lookupCallbackInformation(pluginCallbackId)
+//
+//                           val args = DartExecutor.DartCallback(context.assets, flutterLoader.findAppBundlePath(), userCallbackInformation)
+//
+//                           val engine = FlutterEngine(context)
+//                           engine.dartExecutor.executeDartCallback(args)
+//
+//                           backgroundMethodChannel = MethodChannel(engine.dartExecutor, "flutter_location_listener#callback")
+
+                    backgroundMethodChannel = FlutterMethodChannel(name: "flutter_location_listener#callback", binaryMessenger: self.binaryMessage)
+                      }
+                
+                }
+        
+            result(["latitude": location?.coordinate.latitude, "longitude": location?.coordinate.longitude])
         break
-      case "currentLocation":
-        let l = location
-        result(l == nil
-                ? nil
-                : ["latitude": l.coordinate.latitude, "longitude": l.coordinate?.longitude])
+            case  "currentLocation":
+//                self.cancellable = stream.subject?.subscribe(on: DispatchQueue.global())
+//                    .sink{s in
+//
+//                        self.location = s
+//                        print("\(s.coordinate.latitude)-\(s.coordinate.longitude)")
+//
+//                                }
+                result(["latitude": location?.coordinate.latitude, "longitude": location?.coordinate.longitude])
         break
       case "stopService":
         stream.stopUpdates()
