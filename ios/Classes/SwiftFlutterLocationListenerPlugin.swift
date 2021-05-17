9f942184639a458fbcdd000c69447e2383b72604
SimoneBressan <simone@kuama.it>
2021-05-17 16:19:09 +0200

    wip

--- ios/Classes/SwiftFlutterLocationListenerPlugin.swift
+++ ios/Classes/SwiftFlutterLocationListenerPlugin.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import ios_location_listener
+import Combine
 
 public class SwiftFlutterLocationListenerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -8,7 +10,47 @@ public class SwiftFlutterLocationListenerPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  let stream = StreamLocation()
+  var cancellable: AnyCancellable? = nil
+  var location: CLLocation? = nil
+    
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    print("handle: \(call.method) \(call.arguments)")
+    switch call.method {
+      case "startService":
+        let args = call.arguments as! Dictionary<String, Any>
+        let pluginCallbackId = args["pluginCallbackId"] as! Int
+        let userCallbackId = args["userCallbackId"] as! Int
+            
+        let preferences = UserDefaults.standard
+        preferences.set(pluginCallbackId, forKey: "pluginCallbackId")
+        preferences.set(userCallbackId, forKey: "userCallbackId")
+        preferences.synchronize()
+        
+        stream.startUpdatingLocations()
+        DispatchQueue.main.async{
+            self.cancellable = self.stream.subject?.sink {
+            s in
+              self.location = s
+              print("\(s.coordinate.latitude)-\(s.coordinate.longitude)")
+          }
+        }
+
+        result(nil)
+        break
+      case "currentLocation":
+        let l = location
+        result(l == nil
+                ? nil
+                : ["latitude": l.coordinate.latitude, "longitude": l.coordinate?.longitude])
+        break
+      case "stopService":
+        stream.stopUpdates()
+        DispatchQueue.main.async {
+          self.cancellable?.cancel()
+        }
+        default:
+            break
+    }
   }
 }
--- ios/flutter_location_listener.podspec
+++ ios/flutter_location_listener.podspec
@@ -15,9 +15,13 @@ A new Flutter project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.platform = :ios, '13.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
+
+  # Project
+  s.frameworks = 'Combine', 'UIKit'
+  s.dependency 'ios-location-listener'
 end
